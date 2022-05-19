d2d7b47254f6ae51785ccdda4cf4e8af0533cf73
Afaq Babar <afaqbabar@gmail.com>
2022-05-19 17:08:50 +0000

    update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
+terraform {
+
+  required_providers {
+    ionoscloud = {
+      source  = "ionos-cloud/ionoscloud"
+      version = "= 6.2.1"
+    }
+  }
+
+  cloud {
+    organization = "ionos"
+
+    workspaces {
+      name = "ionos_githubActions"
+    }
+  }
+}
+
 module "dc" {
   source                 = "./dc"
   dc_name                = var.dc_name
@@ -5,3 +23,4 @@ module "dc" {
   dc_description         = var.dc_description
   dc_sec_auth_protection = var.dc_sec_auth_protection
 }
+
