71e8fbeb71370c0f116f82237dbb19d472a02c38
Afaq Babar <afaqbabar@gmail.com>
2022-05-19 05:50:52 +0000

    k8s.tf to add dc-02

--- k8s.tf
+++ k8s.tf
@@ -1,6 +1,6 @@
 resource "ionoscloud_datacenter" "afaq_dc" {
-  name                  = "Afaq Datacenter-02"
-  location              = "de/fra"
-  description           = "Test datacenter for K8s and other resources using Terraform & Github Actions"
-  sec_auth_protection   = false
+  name                = "Afaq Datacenter-02"
+  location            = "de/fra"
+  description         = "Test datacenter for K8s and other resources using Terraform & Github Actions"
+  sec_auth_protection = false
 }
