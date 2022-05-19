02fc50fddb0bc43b30f6f800304883127e59acaa
Afaq Babar <afaqbabar@gmail.com>
2022-05-19 17:03:03 +0000

    introduce modularization

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "dc" {
-  source = "./dc"
-  dc_name = var.dc_name
+  source                 = "./dc"
+  dc_name                = var.dc_name
   dc_location            = var.dc_location
   dc_description         = var.dc_description
   dc_sec_auth_protection = var.dc_sec_auth_protection
