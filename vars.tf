2626a8b10e5a246ab823a5dc0145b5894dc29acd
Afaq Babar <afaqbabar@gmail.com>
2022-05-19 16:44:54 +0000

    vars.tf updated

--- vars.tf
+++ vars.tf
@@ -1,15 +1,15 @@
 variable "dc_name" {
   description = "Name of DC"
-  default = "DC for k8s - 02"
+  default     = "DC for k8s - 02"
 }
 
 variable "dc_location" {
   description = "The regional location where VDC will be created"
-  default = "de/fra"
+  default     = "de/fra"
 }
 
 variable "dc_description" {
   description = "Description of DC"
-  default = "DC for k8s - 02"
+  default     = "DC for k8s - 02"
 }
 
