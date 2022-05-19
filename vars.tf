15ad573307fcc7a7d151fa20c73af49bd0e44356
Afaq Babar <afaqbabar@gmail.com>
2022-05-19 16:43:13 +0000

    vars.tf updated

--- vars.tf
+++ vars.tf
@@ -1,12 +1,15 @@
 variable "dc_name" {
-  description = "Amount of CCE Cluster"
+  description = "Name of DC"
+  default = "DC for k8s - 02"
 }
 
 variable "dc_location" {
   description = "The regional location where VDC will be created"
+  default = "de/fra"
 }
 
 variable "dc_description" {
   description = "Description of DC"
+  default = "DC for k8s - 02"
 }
 
