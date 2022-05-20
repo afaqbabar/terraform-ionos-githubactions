812aedec7b6cb2de968cbffad40482ae107840f3
Afaq Babar <afaqbabar@gmail.com>
2022-05-20 15:10:32 +0000

    output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "host" {
   description = "Output Host"
   value       = data.ionoscloud_k8s_cluster.k8s_cluster_01.config[0].clusters[0].cluster.server
-  sensitive   = false
+  sensitive   = true
 }
 
