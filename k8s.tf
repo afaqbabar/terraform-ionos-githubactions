21de1609c3b1b867a973735300d8bb7cac52cea0
Afaq Babar <afaqbabar@gmail.com>
2022-05-21 05:18:13 +0000

    k8s.tf

--- k8s.tf
+++ k8s.tf
@@ -72,9 +72,9 @@ resource "ionoscloud_k8s_node_pool" "k8s_node_pool_01" {
 
 
 provider "kubernetes" {
-  host  = data.ionoscloud_k8s_cluster.k8s_cluster_01.config[0].clusters[0].cluster.server
-  token = data.ionoscloud_k8s_cluster.k8s_cluster_01.config[0].users[0].user.token
-  cluster_ca_certificate =  data.ionoscloud_k8s_cluster.k8s_cluster_01.config[0].clusters[0].certificate-authority-data
+  host                   = data.ionoscloud_k8s_cluster.k8s_cluster_01.config[0].clusters[0].cluster.server
+  token                  = data.ionoscloud_k8s_cluster.k8s_cluster_01.config[0].users[0].user.token
+  cluster_ca_certificate = data.ionoscloud_k8s_cluster.k8s_cluster_01.config[0].clusters[0].certificate-authority-data
 }
 
 
